5b119787312c8c640e5cbc1ef7592eb08fa21552
ftd6435 <mamadoupathediallo71@gmail.com>
2025-10-17 03:21:46 +0000

    Ajustement de fournisseur resource

---
+++ app/Modules/Fixing/Models/Fixing.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Modules\Fixing\Models;
+
+use App\Modules\Administration\Models\Fournisseur;
+use App\Modules\Administration\Models\User;
+use App\Modules\Settings\Models\Devise;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Fixing extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'fournisseur_id',
+        'poids_pro',
+        'carrat_moyenne',
+        'discount',
+        'bourse',
+        'unit_price',
+        'devise_id',
+        'status',
+        'created_by',
+        'updated_by',
+    ];
+
+    public function fournisseur(): BelongsTo
+    {
+        return $this->belongsTo(Fournisseur::class)->whereNull('fournisseurs.deleted_at');
+    }
+
+    public function devise(): BelongsTo
+    {
+        return $this->belongsTo(Devise::class);
+    }
+
+    public function createdBy(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function updatedBy(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'updated_by');
+    }
+}
