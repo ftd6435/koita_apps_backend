396e59e7ae6179b787d89ff497c0697fee46dd2c
sylladev1805 <ousmanespatec@gmail.com>
2025-10-23 22:04:39 +0000

    mise a jour

--- app/Modules/Fondation/Services/FondationDubaiService.php
+++ app/Modules/Fondation/Services/FondationDubaiService.php
@@ -30,7 +30,7 @@ class FondationDubaiService
                     // 🔹 Mise à jour du statut de la livraison liée
                     if ($fondation->initFondation) {
                         $initLivraison = $fondation->initFondation;
-                        $initLivraison->update(['statut' => 'terminer']);
+                        $initLivraison->update(['status' => 'terminer']);
                     }
 
                     $updated[] = $fondation;
