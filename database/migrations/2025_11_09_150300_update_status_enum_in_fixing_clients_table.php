952a6ac1d57ed12429d41957e1a6f0f741fd1240
sylladev1805 <ousmanespatec@gmail.com>
2025-11-11 23:46:47 +0000

    mises a jour

--- database/migrations/2025_11_09_150300_update_status_enum_in_fixing_clients_table.php
+++ database/migrations/2025_11_09_150300_update_status_enum_in_fixing_clients_table.php
@@ -9,11 +9,16 @@ return new class extends Migration
 {
     public function up(): void
     {
+        // 🔹 Convertir toutes les anciennes valeurs invalides vers une valeur par défaut
+        DB::statement("UPDATE fixing_clients SET status = 'provisoire' WHERE status NOT IN ('provisoire', 'vendu')");
+
+        // 🔹 Modifier l'énumération
         DB::statement("ALTER TABLE fixing_clients MODIFY status ENUM('provisoire', 'vendu') DEFAULT 'provisoire'");
     }
 
     public function down(): void
     {
+        DB::statement("UPDATE fixing_clients SET status = 'en attente' WHERE status NOT IN ('en attente', 'confirmer', 'valider')");
         DB::statement("ALTER TABLE fixing_clients MODIFY status ENUM('en attente', 'confirmer', 'valider') DEFAULT 'en attente'");
     }
 };
